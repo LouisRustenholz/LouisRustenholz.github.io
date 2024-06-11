7328231d0b234cd5ba592b595e29ec33363c6965
Louis Rustenholz <rustenholz.louis@gmail.com>
2024-06-11 15:39:38 +0200

    Updates in biblio, default darkmode

--- _pages/about.md
+++ _pages/about.md
@@ -11,9 +11,11 @@ profile:
   more_info:
 
 news: false # includes a list of news items
-selected_papers: true # includes a list of papers marked as "selected={true}"
+selected_papers: false # includes a list of papers marked as "selected={true}"
 social: true # includes social icons at the bottom of the page
-all_papers: true
+main_papers: true
+service: true
+extra_papers: true
 ---
 
 Write your biography here. Tell the world about yourself. Link to your favorite [subreddit](http://reddit.com). You can put a picture in, too. The code is already in, just name your picture `prof_pic.jpg` and put it in the `img/` folder.
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -207,28 +207,38 @@ let transTheme = () => {
 
 // Determine the expected state of the theme toggle, which can be "dark", "light", or
 // "system". Default is "system".
+// let determineThemeSetting = () => {
+//   let themeSetting = localStorage.getItem("theme");
+//   if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
+//     themeSetting = "system";
+//   }
+//   return themeSetting;
+// };
+
+// Always use darkmode instead
 let determineThemeSetting = () => {
-  let themeSetting = localStorage.getItem("theme");
-  if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
-    themeSetting = "system";
-  }
-  return themeSetting;
+    return "dark";
 };
 
 // Determine the computed theme, which can be "dark" or "light". If the theme setting is
 // "system", the computed theme is determined based on the user's system preference.
+// let determineComputedTheme = () => {
+//   let themeSetting = determineThemeSetting();
+//   if (themeSetting == "system") {
+//     const userPref = window.matchMedia;
+//     if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+//       return "dark";
+//     } else {
+//       return "light";
+//     }
+//   } else {
+//     return themeSetting;
+//   }
+// };
+
+// Always use darkmode instead
 let determineComputedTheme = () => {
-  let themeSetting = determineThemeSetting();
-  if (themeSetting == "system") {
-    const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    } else {
-      return "light";
-    }
-  } else {
-    return themeSetting;
-  }
+    return "dark";
 };
 
 let initTheme = () => {
